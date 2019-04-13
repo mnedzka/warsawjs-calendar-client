156d7520754e0348dfe26b22cdc9fac773433705
Michal Nedzka <michael.nedzka@gmail.com>
2019-04-13 22:32:56 +0200

    Connect day to redux

---
+++ src/components/day/DayConnect.tsx
@@ -0,0 +1,39 @@
+import { Moment } from 'moment';
+import { connect } from 'react-redux';
+import { dispatchRefetchCalendar } from '../../redux/actions/calendarActions';
+import {
+  dispatchDeleteEvent,
+  dispatchFetchDay,
+  dispatchUpdateEvent,
+  IDayEvent,
+} from '../../redux/actions/dayActions';
+import { IRootReducerState } from '../../redux/reducers/rootReducer';
+import Day from './Day';
+
+const mapStateToProps = (state: IRootReducerState) => {
+  const { day } = state;
+
+  return day;
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    dispatchFetchDay(selectedDay: Moment) {
+      dispatch(dispatchFetchDay(selectedDay.format('YYYY-MM-DD')));
+    },
+    dispatchDeleteEvent(eventId: string) {
+      return dispatch(dispatchDeleteEvent(eventId));
+    },
+    dispatchUpdateEvent(data: IDayEvent) {
+      return dispatch(dispatchUpdateEvent(data));
+    },
+    dispatchRefetchCalendar() {
+      dispatch(dispatchRefetchCalendar());
+    },
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Day);
