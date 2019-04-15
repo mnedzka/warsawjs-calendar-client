6fbcb3248152d58f42fd4ee84860dc2f7e585738
Michal Nedzka <michael.nedzka@gmail.com>
2019-04-15 21:15:38 +0200

    Add root reducer

---
+++ src/redux/reducers/rootReducer.ts
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import calendarReducer, { ICalendarReducerState } from './calendarReducer';
+import dayReducer, { IDayReducerState } from './dayReducer';
+
+export interface IRootReducerState {
+  calendar: ICalendarReducerState;
+  day: IDayReducerState;
+}
+
+const rootReducer = combineReducers<IRootReducerState>({
+  calendar: calendarReducer,
+  day: dayReducer,
+});
+
+export default rootReducer;
